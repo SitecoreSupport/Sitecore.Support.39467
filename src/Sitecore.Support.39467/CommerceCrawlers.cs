c83a079de660d4ca7a733a2fafe0ce3866813aae
MichaelToh <ttk@sitecore.net>
2018-06-20 13:42:10 +0800

    Initial commit

---
+++ src/Sitecore.Support.39467/CommerceCrawlers.cs
@@ -0,0 +1,45 @@
+﻿namespace Sitecore.Support.Commerce.Engine.Connect.Search.Crawlers
+{
+  using System.Threading;
+  using Sitecore.ContentSearch;
+  using Sitecore.Diagnostics;
+
+  public class SellableItemsCrawler : Sitecore.Commerce.Engine.Connect.Search.Crawlers.SellableItemsCrawler
+  {
+    public override void RefreshFromRoot(IProviderUpdateContext context, IIndexable indexableStartingPoint, IndexingOptions indexingOptions, CancellationToken cancellationToken)
+    {
+      // This is the copy of the base method needed here to avoid ERROR: indexableStartingPoint SitecoreIndexableItem is not an instance of CommerceCatalogIndexableItem
+      Assert.ArgumentNotNull(context, "context");
+      if (base.ShouldStartIndexing(indexingOptions))
+      {
+        this.Update(context, cancellationToken);
+      }
+    }
+  }
+
+  public class CategoriesCrawler : Sitecore.Commerce.Engine.Connect.Search.Crawlers.CategoriesCrawler
+  {
+    public override void RefreshFromRoot(IProviderUpdateContext context, IIndexable indexableStartingPoint, IndexingOptions indexingOptions, CancellationToken cancellationToken)
+    {
+      // This is the copy of the base method needed here to avoid ERROR: indexableStartingPoint SitecoreIndexableItem is not an instance of CommerceCatalogIndexableItem
+      Assert.ArgumentNotNull(context, "context");
+      if (base.ShouldStartIndexing(indexingOptions))
+      {
+        this.Update(context, cancellationToken);
+      }
+    }
+  }
+
+  public class CatalogsCrawler : Sitecore.Commerce.Engine.Connect.Search.Crawlers.CatalogsCrawler
+  {
+    public override void RefreshFromRoot(IProviderUpdateContext context, IIndexable indexableStartingPoint, IndexingOptions indexingOptions, CancellationToken cancellationToken)
+    {
+      // This is the copy of the base method needed here to avoid ERROR: indexableStartingPoint SitecoreIndexableItem is not an instance of CommerceCatalogIndexableItem
+      Assert.ArgumentNotNull(context, "context");
+      if (base.ShouldStartIndexing(indexingOptions))
+      {
+        this.Update(context, cancellationToken);
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.39467/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.39467")]
+[assembly: AssemblyProduct("Sitecore.Support.39467")]
+[assembly: ComVisible(false)]
